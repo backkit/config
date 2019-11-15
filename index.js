c8044fd221ae0567271e76bb67cc2260c03c6d71
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2019-11-15 16:57:00 +0100

    use logger service if exists

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const camelCase = require('camelcase');
 const yaml = require('js-yaml');
 
 class Config {
-  constructor() {
+  constructor(services) {
     this.configs = null;
+    this.logfn = console.log;
+    // attempt to use logger service if exists, to have more consistent logs across the app
+    // otherwise, fallback to console.log
+    try {
+      this.logfn = services.logger.info;
+    } catch (ex) {}
   }
 
   loadConfigs() {
@@ -13,7 +19,7 @@ class Config {
       fs.readdirSync('./config').forEach(file => {
         if (file.endsWith(".yml")) {
           const name = camelCase(file.substr(0, file.length - 4));
-          console.log(`loading config from ./config/${file} as "${name}"`);
+          this.logfn(`loading config from ./config/${file} as "${name}"`);
           this.configs[name] = yaml.safeLoad(fs.readFileSync(`./config/${file}`, 'utf8'));
         }
       });

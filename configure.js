13750c3596f9cfafe360f7a742d9c16884b0f22b
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2023-04-02 03:26:29 +0400

    migrate to @backkit/autoconf

--- configure.js
+++ configure.js
@@ -1,34 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-const inquirer = require('inquirer');
-const yaml = require('js-yaml');
+const autoconf = require("@backkit/autoconf");
 
-const skipAutoconf = process.env.NO_AUTOCONF ? true : false;
-
-const generate = (serviceName, moduleName, config) => {
-  const serviceDir = `${__dirname}/../../services`;
-  const servicePath = `${__dirname}/../../services/${serviceName}.js`;
-  const configDir = `${__dirname}/../../config`;
-
-  // save service config
-  console.log(`creating config folder: ${configDir}`);
-  if (!fs.existsSync(configDir)) {
-    fs.mkdirSync(configDir, {recursive: true});
+autoconf('config')
+.generator(self => ([
+  {
+    mkdirp: self.rootServiceDir
+  },
+  {
+    putFileOnce: self.serviceCodeMainJS,
+    content: `module.exports = require('${self.npmModuleName}')`
   }
+]))
+.default(self => ({}))
+.prompt(self => ([]))
+.run()
 
-  // enable service
-  console.log(`creating service alias: ${servicePath}`);
-  if (!fs.existsSync(serviceDir)) {
-    fs.mkdirSync(serviceDir, {recursive: true});
-  }
-  if (!fs.existsSync(servicePath)) {
-    fs.writeFileSync(servicePath, `module.exports = require('${moduleName}')`);
-  }
-};
-
-if (!skipAutoconf) {
-  const packageJson = require('./package.json');
-  const serviceName = 'config';
-  const moduleName = packageJson.name;
-  generate(serviceName, moduleName, {});
-}
